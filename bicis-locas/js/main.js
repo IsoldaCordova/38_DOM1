91919e9ff2d2b78931ca1274adbedc65d1a89932
IsoldaCordova <Isolda.Cordova@gmail.com>
2016-10-18 09:48:17 -0500

    creo que ya quedo el de las bicis

--- bicis-locas/js/main.js
+++ bicis-locas/js/main.js
@@ -5,8 +5,8 @@ function validateForm(){
 	if( name == null || name == ""){
 		alert("Por Favor Ingresa tu Nombre");
 		return false;
-	}else if (!/^[a-z A-Z]*$/g.test (document.getElementById("name").value)){
-		alert("caracteres invalidos");
+	}else if (!/^[A-Z][a-z]*$/g.test (document.getElementById("name").value)){
+		alert("caracteres invalidos, poner la primera en Mayúscula");
 		return false;
 	};
 	function MaysPrimera(string){
@@ -18,24 +18,37 @@ function validateForm(){
 	if( lastname == null || lastname == ""){
 		alert("Por Favor Ingresa tu Apellido");
 		return false;
+	}else if (!/^[A-Z][a-z]*$/g.test (document.getElementById("name").value)){
+		alert("caracteres invalidos, poner la primera en Mayúscula");
+		return false;
 	};
 	// email
 	var email = document.getElementById("input-email").value;
 	if( email == null || email == ""){
 		alert("Por Favor Ingresa tu Correo");
 		return false;
-	} else if ( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(email)) ) {
-      alert("fake!!");
-      return false;
-     }
+	};
+	var email = 'me@example.com';
+	validarEmail( email );
+	function validarEmail( email ) {
+    	expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    	if ( !expr.test(email) )
+        alert("Error: La dirección de correo " + email + " es incorrecta.");
+	};
      // contraseña
 	var contrasena = document.getElementById("input-password").value;
-	if( contrasena == null || constrasena == ""){
+	if( contrasena == null || contrasena == ""){
 		alert("Por Favor Ingresa tu Password");
 		return false;
-	} else if (contrasena < 6){
+	} else if (contrasena.length < 6){
 		alert("La contraseña debe tener al menos 6 caracteres")
 	}else if ((contrasena == 123456 ) || (contrasena == 098754) || (contrasena == "password")){
 		alert("no se puede, cambia password");
+	};
+	// seleccion
+	var bici = document.getElementById("lista").selectedIndex;
+	if( bici == null || bici == 0){
+		alert("Por Favor Selecciona tipo de bici");
+		return false;
 	}
-};
+}
